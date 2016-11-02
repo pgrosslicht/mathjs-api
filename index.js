4bb279636577f6265608d710294dd1874987065c
Patrick Grosslicht <patrick@grosslicht.com>
2016-11-02 20:25:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by patrickgrosslicht on 02/11/16.
+ */
+
+var restify = require('restify');
+var math = require('mathjs');
+
+function respond(req, res, next) {
+    res.send(200, {"result": math.format(math.eval(req.body.expr))});
+    next();
+}
+
+var server = restify.createServer({});
+server.use(restify.bodyParser());
+server.post('/eval', respond);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
